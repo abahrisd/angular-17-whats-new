29aa0b4d7f18990dbda9430595afcb88e4feaf09
Samir Abakhri <abakhri.sd@gmail.com>
2024-02-10 15:30:26 +0500

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-17-whats-new'
+  links = [
+    {
+      routerLink: '/blocks',
+      name: 'Blcoks',
+    },
+    {
+      routerLink: '/',
+      name: 'Welcome',
+    },
+  ]
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { PageNotFoundComponent } from './feature/page-not-found/page-not-found.component'
+import { WelcomeComponent } from './feature/welcome/welcome.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'blocks',
+    loadChildren: () =>
+      import('./feature/blocks/blocks.module').then((m) => m.BlocksModule),
+    data: { preload: true },
+  },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: './welcome', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+]
