ee5e1548caf3337725cd6e5dd9d5041cc886d898
Samir Abakhri <abakhri.sd@gmail.com>
2024-02-12 02:10:49 +0500

    move navigation to side-navigation drawer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core'
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router'
 import { CommonModule } from '@angular/common'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { SideNavigationComponent } from './shared/side-navigation/side-navigation.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    SideNavigationComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -21,4 +32,18 @@ export class AppComponent {
       name: 'Welcome',
     },
   ]
+
+  isNavigationOpened = false
+
+  navigationConfig = [
+    { title: 'Welcome', routerLink: '/welcome', icon: 'home' },
+    { title: 'Blocks', routerLink: '/blocks', icon: 'apps' },
+    { title: 'Login', routerLink: '/login', icon: 'login' },
+    { title: 'Admin', routerLink: '/admin', icon: 'settings_applications' },
+  ]
+
+  public toggleNavigationOpenedState() {
+    console.log('toggleNavigationOpenedState')
+    this.isNavigationOpened = !this.isNavigationOpened
+  }
 }
--- src/app/shared/side-navigation/side-navigation.component.ts
+++ src/app/shared/side-navigation/side-navigation.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
+import { MatListModule } from '@angular/material/list'
+import { MatIconModule } from '@angular/material/icon'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { NavigationListItem } from './side-navigation.model'
+import { RouterLink, RouterLinkActive } from '@angular/router'
 
 @Component({
   selector: 'awn-side-navigation',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './side-navigation.component.html',
   styleUrl: './side-navigation.component.css',
 })
-export class SideNavigationComponent {}
+export class SideNavigationComponent {
+  @Input() navigationConfig: NavigationListItem[] = []
+
+  @Input() isOpened = false
+}
